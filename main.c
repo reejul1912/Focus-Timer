90e7a3865d88f6395aae08f82df097d977a206b7
Reejul Kant <brainbulb29@gmail.com>
2026-05-14 01:48:24 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -4,20 +4,20 @@
 #include <conio.h>
 #include <stdbool.h>
 
-void window_size (int width,int height) {
+void window_size (int width,int height) { 		//setting window size
 	SetConsoleTitle("Focus Timer By Reejul");
     HWND console=GetConsoleWindow();
     RECT r;
     GetWindowRect(console, &r); 
     MoveWindow(console, r.left, r.top, width, height, TRUE);
     long style=GetWindowLong(console, GWL_STYLE);
-    style&=~WS_MAXIMIZEBOX; 
-    style&=~WS_THICKFRAME;  
+    style&=~WS_MAXIMIZEBOX;  	//disabling maximize button
+    style&=~WS_THICKFRAME;  	//disabling window resizing
     SetWindowLong(console,GWL_STYLE,style);
 }
 
-void load_font_size(int size) {
-    HANDLE text = GetStdHandle(STD_OUTPUT_HANDLE);
+void load_font_size(int size) {			//setting text size
+    HANDLE text = GetStdHandle(STD_OUTPUT_HANDLE);   
     CONSOLE_FONT_INFOEX cfi;
     cfi.cbSize = sizeof(cfi);
     cfi.nFont = 0;
@@ -33,7 +33,7 @@ void hide_cursor() {
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO info;
     GetConsoleCursorInfo(consoleHandle, &info);
-    info.bVisible = FALSE;
+    info.bVisible = FALSE;   //turning that blinking cursor on
     SetConsoleCursorInfo(consoleHandle, &info);
 }
 
@@ -41,17 +41,17 @@ void show_cursor() {
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO info;
     GetConsoleCursorInfo(consoleHandle, &info);
-    info.bVisible = TRUE;
+    info.bVisible = TRUE;    //turning that blinking cursor back on
     SetConsoleCursorInfo(consoleHandle, &info);
 }
 
-void audio() {
+void audio() {		//loading all the sounds
 	mciSendString("open \"enter.mp3\" type mpegvideo alias enter", NULL, 0, NULL);
 	mciSendString("open \"lofi.mp3\" type mpegvideo alias lofi", NULL, 0, NULL);
 	mciSendString("open \"rain.mp3\" type mpegvideo alias rain", NULL, 0, NULL);
 }
 
-void main_menu() {
+void main_menu() {    //making the main menu using ASCII
 	HANDLE text_colour = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(text_colour, 11);
     printf("\n");
@@ -71,7 +71,7 @@ void main_menu() {
     system("cls");
 }
 
-void draw_mountains() {
+void draw_mountains() {			//mountains ascii art
 	HANDLE text_colour = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(text_colour, 2);
     printf("           /\\ \n");
@@ -82,9 +82,9 @@ void draw_mountains() {
     printf("  /    \\         \\   \\/      \\ \n");
     printf(" /      \\         \\  /        \\ \n");
     SetConsoleTextAttribute(text_colour, 3);
-    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); //river art
 }
-	void music_selection() {
+	void music_selection() {		//music selection menu
 		HANDLE text_colour = GetStdHandle (STD_OUTPUT_HANDLE);
 		SetConsoleTextAttribute(text_colour, 13);
 		
@@ -115,89 +115,94 @@ void draw_mountains() {
 
 int main(void) {
 	
-		window_size(550, 325);
-		load_font_size(24);
+		window_size(550, 325); //width * height of window
+		load_font_size(24);  //24 is the font size
 	
 		HANDLE text_colour = GetStdHandle(STD_OUTPUT_HANDLE);
-		int bar, hour, min, sec, i, j, k, final_hour, final_min, final_sec;
+		int bar, hour, min, sec, i, j, k, final_hour, final_min, final_sec; //couldnt declare variables inside loop due to C89
 		char again;
 		
 		do{
-		system("cls");
-		main_menu();
-		audio();
-		mciSendString("play enter from 0", NULL, 0, NULL);
+			system("cls");
+			main_menu(); //main menu appears
+			audio(); 
+			mciSendString("play enter from 0", NULL, 0, NULL);
 		
-		for (bar = 0; bar <= 30; bar++) {
-			printf ("=");
-			Sleep(20);
+			for (bar = 0; bar <= 30; bar++) {
+				printf ("="); //loading bar animation
+				Sleep(20);
+			}
+		
+			while (true) { //infinite loop for invalid inputs
+				
+				SetConsoleTextAttribute(text_colour, 7);
+				printf("\n Enter the time in format [HOURS:MINS]: ");
+				if (scanf("%d:%d", &hour, &min) == 2 && hour >= 0 && min >= 0 && min < 60) {
+					break;
+			}
+			
+			else { //invalid input
+			
+		    	mciSendString("play enter from 0", NULL, 0, NULL);
+				SetConsoleTextAttribute(text_colour, 12);
+				printf("Invalid time. Enter it like 2:12, 0:30 etc. \n");
+				while (getchar() != '\n');
+			}
 		}
 		
-		while(true){
-		SetConsoleTextAttribute(text_colour, 7);
-		printf("\n Enter the time in format [HOURS:MINS]: ");
-		if (scanf("%d:%d", &hour, &min) == 2 && hour >= 0 && min >= 0 && min < 60) {
-			break;
-		}
-		else { // setting infinte loop for invalid inputs
-		    mciSendString("play enter from 0", NULL, 0, NULL);
-			SetConsoleTextAttribute(text_colour, 12);
-			printf("Invalid time. Enter it like 2:12, 0:30 etc. \n");
-			while (getchar() != '\n');
-		}
-		}
+			mciSendString("play enter from 0", NULL, 0, NULL);
+			sec = ((hour * 3600) + (min * 60));
 		
-		mciSendString("play enter from 0", NULL, 0, NULL);
-		sec = ((hour * 3600) + (min * 60));
+			system("cls"); //screen clears. I know this is not the best way to do it but it is what I know
 		
-		system("cls");
+			music_selection(); // music selection menu appears
 		
-		music_selection(); // music selection menu appears
+			SetConsoleTextAttribute(text_colour, 7);
+			printf("\n\n Timer set for %d hours %d minutes.\n", hour, min);
+			SetConsoleTextAttribute(text_colour, 14);
+			printf(" You can press E to exit the timer.\n\n");
 		
-		SetConsoleTextAttribute(text_colour, 7);
-		printf("\n\n Timer set for %d hours %d minutes.\n", hour, min);
-		SetConsoleTextAttribute(text_colour, 14);
-		printf(" You can press E to exit the timer.\n\n");
+			draw_mountains(); //dislays the ASCII art of mountains
+			hide_cursor(); 
 		
-		draw_mountains();
-		hide_cursor();
-		
-		SetConsoleTextAttribute(text_colour, 9);
-		for (i = sec; i >= 0; i--) {
+			SetConsoleTextAttribute(text_colour, 9);
+			for (i = sec; i >= 0; i--) { 
 					
-			if (kbhit()) {
-        	char key = _getch();
-        	if (key == 'E' || key == 'e') {
-            break; 
-        }
+			if (kbhit()) { // user presses E key to exit the timer
+        		char key = _getch();
+        		if (key == 'E' || key == 'e') {
+            	break; 
+        	}
     	}
+    		//converting seconds into hh:mm:ss
 			final_hour = (i / 3600);
 			final_min = ((i % 3600) / 60);
 			final_sec = (i % 60);
 			
 			if (i <= 10) {
-        	SetConsoleTextAttribute(text_colour, 12);
+        		SetConsoleTextAttribute(text_colour, 12);
     		} else {
-        	SetConsoleTextAttribute(text_colour, 9);
+        		SetConsoleTextAttribute(text_colour, 9);
     		}
-			printf ("\rTime left: %02d:%02d:%02d", final_hour, final_min, final_sec);
-			Sleep(1000);
+				printf ("\rTime left: %02d:%02d:%02d", final_hour, final_min, final_sec);
+				Sleep(1000);
 		}
 		
-		mciSendString("close lofi", NULL, 0, NULL);
-        mciSendString("close rain", NULL, 0, NULL);
+			mciSendString("close lofi", NULL, 0, NULL);
+        	mciSendString("close rain", NULL, 0, NULL);
         
-        system("cls");
-        show_cursor();
-		SetConsoleTextAttribute(text_colour, 13);
-		mciSendString("play enter from 0", NULL, 0, NULL);
-		printf("\n Timer stopped at %02d:%02d:%02d\n\n", final_hour, final_min, final_sec);
-		SetConsoleTextAttribute(text_colour, 7);
-		printf(" Would you like to set another timer? [Y/N]: ");
-		scanf(" %c", &again);
-		mciSendString("play enter from 0", NULL, 0, NULL);
+        	system("cls");
+        	show_cursor();
+        	
+			SetConsoleTextAttribute(text_colour, 13);
+			mciSendString("play enter from 0", NULL, 0, NULL);
+			printf("\n Timer stopped at %02d:%02d:%02d\n\n", final_hour, final_min, final_sec);
+			SetConsoleTextAttribute(text_colour, 7);
+			printf(" Would you like to set another timer? [Y/N]: ");
+			scanf(" %c", &again);
+			mciSendString("play enter from 0", NULL, 0, NULL);
 		
-	} while(again == 'Y' || again == 'y');
+		} while(again == 'Y' || again == 'y'); // goto menu when user presses y
 	
 	printf("\n Thank you for using Focus Timer!");
 	Sleep(1500);
